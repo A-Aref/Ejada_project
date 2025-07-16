2224f7cd57e9e81afae0211a732760271f4509a4
A-Aref <abdallahahmadaref@gmail.com>
2025-07-16 12:14:38 +0300

    first commit

---
+++ Accounts/src/test/java/com/ejada/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejada.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
