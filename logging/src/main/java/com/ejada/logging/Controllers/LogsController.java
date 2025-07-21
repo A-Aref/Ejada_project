d8da4785c14dec75735bede594ca25353347b68b
A-Aref <abdallahahmadaref@gmail.com>
2025-07-21 16:39:51 +0300

    Added error check for logs controller

--- logging/src/main/java/com/ejada/logging/Controllers/LogsController.java
+++ logging/src/main/java/com/ejada/logging/Controllers/LogsController.java
@@ -3,11 +3,14 @@ package com.ejada.logging.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ejada.logging.Models.LogsModel;
 import com.ejada.logging.Services.LogsService;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -19,11 +22,16 @@ public class LogsController {
     private LogsService logsService;
 
     @GetMapping("/")
-    public HashMap<String,Object> getLogs() {
+    public ResponseEntity<HashMap<String,Object>> getLogs() {
         HashMap<String, Object> response = new HashMap<>();
         response.put("message", "Logs retrieved successfully");
+        List<LogsModel> logs = logsService.getLogs();
+        if (logs.isEmpty()) {
+            response.put("message", "No logs found");
+            return ResponseEntity.status(404).body(response);
+        }
         response.put("logs", logsService.getLogs());
-        return response;
+        return ResponseEntity.status(200).body(response);
     }
     
 
